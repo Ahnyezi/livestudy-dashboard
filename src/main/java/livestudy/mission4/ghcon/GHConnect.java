711db645d447f10654ddae437fd561936da2d2b5
52pickl <anyeji1220@gmail.com>
2020-12-05 15:55:58 +0900

    gif

--- src/main/java/livestudy/mission4/ghcon/GHConnect.java
+++ src/main/java/livestudy/mission4/ghcon/GHConnect.java
@@ -6,7 +6,8 @@ import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 public class GHConnect {
-	private static final String personalToken = "Insert your personal token";
+//	private static final String personalToken = "Insert your personal token";
+	private static final String personalToken = "a3d6b51b10bfc360d4005c03bc8c9125c1a78422";
 	private static final Logger LOG = Logger.getGlobal();
 	private GitHub github;
 
